8689a2f1791d634027cc6cb3d3dbd95364eae55b
deviant <deviant@devlap.localdomain>
2013-05-26 19:02:25 -0700

    Updating to 3.0.13 release

--- ftp.sh
+++ ftp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 # GamePanelX
-# FTP Install v3.0
+# FTP Install v3.0.14
 #
 # This script supports dependency detection on RedHat/CentOS/Fedora, Debian/Ubuntu, and Gentoo
+# Note: As of 3.0.14, MySQL is no longer needed.  System users only; e.g. basic install.
 #
 # Usage: ./ftp.sh -u someuser
 #
@@ -33,49 +34,19 @@ then
 	exit
 fi
 
-# Get Homedir
-gpx_user_home="$(eval echo ~$gpx_user)"
-
-# Get UID and GID for FTP mysql.conf
-avail_uid="$(grep $gpx_user /etc/passwd | awk -F: '{print $3}')"
-avail_gid="$(grep $gpx_user /etc/group | awk -F: '{print $3}')"
-
-if [[ "$gpx_user_home" == "" || "$avail_uid" == "" || "$avail_gid" == "" ]]
-then
-	echo "FTP ERROR: Required values were left out.  Exiting."
-	exit
-fi
-
-echo
-read -p "Master Server MySQL Server IP Address: " gpx_master_ip
-read -p "Master Server MySQL Server Port: " gpx_master_mysql_port
-read -p "Master Server MySQL Server Database Name: " gpx_master_mysql_db
-read -p "Master Server MySQL Server Database Username: " gpx_master_mysql_user
-read -p "Master Server MySQL Server Database Password: " gpx_master_mysql_pass
-
-echo
-echo "##################################################################"
-echo
-
 ##############################################################
 
-# Check required mysql
-if [[ "$gpx_user" == "" || "$gpx_master_ip" == "" || "$gpx_master_mysql_port" == "" || "$gpx_master_mysql_db" == "" || "$gpx_master_mysql_user" == "" || "$gpx_master_mysql_pass" == "" ]]
-then
-    echo "Required MySQL fields were left empty! Exiting."
-    exit
-fi
-
 # Check for a running FTP server
-if [ "$(netstat -an | awk '{print $4,$6}' | grep ':21' | grep LISTEN)" ]
-then
-        ftp_out="$(netstat -an | grep ':21' | grep LISTEN)"
+ftp_out="$(netstat -an | grep ':21' | grep LISTEN)"
 
-        echo;echo
+if [ "$ftp_out" ]
+then
+        echo
+        echo
         echo -e "\e[00;31mFTP ERROR: There is already a running FTP server on port 21:\e[00m"
         echo "$ftp_out"
         echo -e "\e[00;31mYou must manually stop the other FTP server first.  SKIPPING FTP server installation.\e[00m"
-	exit
+        exit
 fi
 
 # Function to check required
@@ -89,14 +60,6 @@ gpx_checkreq () {
 	    yum_cmd="gcc kernel-headers"
 	    apt_cmd="build-essential"
 	    gentoo_cmd="sys-devel/gcc"
-	fi
-
-	# Check MySQL
-	if [[ "$(which mysql 2>&1 | grep 'no mysql in')" || "$(which mysql_config 2>&1 | grep 'no mysql_config in')" ]]
-	then
-	    yum_cmd=$yum_cmd" mysql mysql-devel"
-	    apt_cmd=$apt_cmd" libmysqlclient-dev"
-	    gentoo_cmd=$gentoo_cmd"  dev-db/mysql"
 	fi
 }
 
@@ -113,7 +76,7 @@ then
 
 		if [[ "$gpx_ok_yum" == "y" || "$gpx_ok_yum" == "yes" || "$gpx_ok_yum" == "Y" ]]
 		then
-			yum -y install $yum_cmd
+        yum -y install $yum_cmd
 		fi
 	fi
 # Debian / Ubuntu
@@ -126,7 +89,7 @@ then
 
 		if [[ "$gpx_ok_apt" == "y" || "$gpx_ok_apt" == "yes" || "$gpx_ok_apt" == "Y" ]]
 		then
-			apt-get --yes install $apt_cmd
+        apt-get --yes install $apt_cmd
 		fi
 	fi
 # Gentoo
@@ -138,9 +101,9 @@ then
 		read -p "(Gentoo) Missing requirements!  Is it OK to install packages via Portage (emerge $gentoo_cmd)? (y/n): " gpx_ok_gentoo
 
 		if [[ "$gpx_ok_gentoo" == "y" || "$gpx_ok_gentoo" == "yes" || "$gpx_ok_gentoo" == "Y" ]]
-                then
-                        emerge $gentoo_cmd
-                fi
+    then
+        emerge $gentoo_cmd
+    fi
 	fi
 fi
 
@@ -149,7 +112,7 @@ gpx_checkreq
 
 if [ "$yum_cmd" ]
 then
-    echo "ERROR: Unable to install the required packages!  Please make sure the GCC and MySQL development libraries are installed and try again."
+    echo "ERROR: Unable to install the required packages!  Please make sure GCC is installed and try again."
     exit
 fi
 
@@ -161,26 +124,26 @@ sleep 1
 ##############################################################
 
 # Prepare FTP Server
-rm -fr ./gpx_tmp_ftpinstall
-mkdir ./gpx_tmp_ftpinstall
-cd ./gpx_tmp_ftpinstall
+rm -fr /usr/local/gpx/ftpd/src
+mkdir -p /usr/local/gpx/ftpd/src
+cd /usr/local/gpx/ftpd/src
 
 # Only download if needed
-if [ ! -f ./gpxpro-ftpd-latest.tar.gz ]
+if [ ! -f ./ftpd-latest.tar.gz ]
 then
-	wget http://gamepanelx.com/files/gpxpro-ftpd-latest.tar.gz
+    wget http://gamepanelx.com/files/ftpd-latest.tar.gz
 fi
 
-if [ ! -f ./gpxpro-ftpd-latest.tar.gz ]
+if [ ! -f ./ftpd-latest.tar.gz ]
 then
-	echo "ERROR: Failed to download the latest FTP Server files!  Exiting."
-	exit
+    echo "ERROR: Failed to download the latest FTP Server files!  Exiting."
+    exit
 fi
 
 # Compile FTP Server
-tar -zxf gpxpro-ftpd-latest.tar.gz
-cd gpxpro-ftpd-latest
-./configure --prefix=$gpx_user_home/ftpd --with-puredb --with-extauth --with-throttling --with-ratios --with-virtualhosts --with-peruserlimits --with-everything --with-mysql
+tar -zxf ftpd-latest.tar.gz
+cd ftpd-latest
+./configure --prefix=/usr/local/gpx/ftpd
 sleep 1
 make
 sleep 1
@@ -188,46 +151,26 @@ make install
 
 ################
 
-if [ ! -f "$gpx_user_home/ftpd/sbin/pure-ftpd" ]
+if [ ! -f "/usr/local/gpx/ftpd/sbin/pure-ftpd" ]
 then
-	echo
-	echo -e "\e[00;31mERROR: The FTP Server installation failed.  Check the output above for why the FTP installation failed. Exiting.\e[00m"
-	exit
+    echo
+    echo -e "\e[00;31mERROR: The FTP Server installation failed (no binary).  Check the output above for why the FTP installation failed. Exiting.\e[00m"
+    exit
 fi
 
 ################
 
-# Setup MySQL for FTP Server
-echo -e "MYSQLSocket             /tmp/mysql.sock
-MYSQLServer             $gpx_master_ip
-MYSQLPort               $gpx_master_mysql_port
-MYSQLUser               $gpx_master_mysql_user
-MYSQLPassword           $gpx_master_mysql_pass
-MYSQLDatabase           $gpx_master_mysql_db
-MYSQLCrypt              md5
-MYSQLGetPW              SELECT password FROM users WHERE username='\L' AND active='1' AND perm_ftp='1'
-MYSQLGetDir             SELECT CONCAT(p.homedir, '/accounts/\L/') AS userdir FROM network AS n LEFT JOIN network AS p ON n.parentid = p.id OR n.id = p.id WHERE n.ip = '\I' LIMIT 1
-MYSQLDefaultUID         $avail_uid
-MYSQLDefaultGID         $avail_gid" > $gpx_user_home/ftpd/mysql.conf
-
-################
-
 # Create startup script
-echo "#!/bin/bash" > $gpx_user_home/ftpd/start.sh
-echo "$gpx_user_home/ftpd/sbin/pure-ftpd -A -B -C 5 -c 150 -E -H -R -x -X -d -j -l mysql:$gpx_user_home/ftpd/mysql.conf" >> $gpx_user_home/ftpd/start.sh
-chmod u+x $gpx_user_home/ftpd/start.sh
+echo '#!/bin/bash' > /usr/local/gpx/ftpd/start.sh
+echo "/usr/local/gpx/ftpd/sbin/pure-ftpd -A -B -C 5 -c 150 -E -H -R -x -X -d -j" >> /usr/local/gpx/ftpd/start.sh
+chmod u+x /usr/local/gpx/ftpd/start.sh
 
 # Security
-chown root: $gpx_user_home/ftpd
-chown root: $gpx_user_home/ftpd -R
-chmod 600 $gpx_user_home/ftpd
-chmod 600 $gpx_user_home/ftpd/mysql.conf
+chown root: /usr/local/gpx/ftpd -R
+chmod 600 /usr/local/gpx/ftpd
 
 # Start the FTP Server
-back_wd=`pwd`
-cd $gpx_user_home/ftpd/
-./start.sh
-cd $back_wd
+/usr/local/gpx/ftpd/start.sh
 sleep 2
 
 # Check if server is running
@@ -235,48 +178,14 @@ ftp_pid="$(ps -ef | grep 'pure-ftpd (SERVER)' | grep -v grep | awk '{print $2}')
 
 if [ "$ftp_pid" ]
 then
-	echo -e "\e[00;32mFTP Server started successfully with PID $ftp_pid! \e[00m"
+    # Save PID
+    echo $ftp_pid > /usr/local/gpx/ftpd/pure-ftpd.pid
+    
+    echo -e "\e[00;32mFTP Server started successfully with PID $ftp_pid! \e[00m"
 else
-	echo -e "\e[00;31mFTP Server failed to start, no running PID found! \e[00m"
-fi
-
-# Test MySQL connection
-mysql_test_gpx="$(mysql $gpx_master_mysql_db --connect-timeout=10 -B -e 'SELECT 1' -h$gpx_master_ip -u$gpx_master_mysql_user -p$gpx_master_mysql_pass  2>&1 | grep ERROR)"
-
-if [ "$mysql_test_gpx" ]
-then
-    # Check for eth0
-    if [ "$(ifconfig | grep eth0)" ]
-    then
-        use_ip="$(ifconfig eth0 | grep 'inet addr' | head -1 | awk '{print $2}' | awk -F':' '{print $2}')"
-    # Virtual
-    elif [ "$(ifconfig | grep venet0:0)" ]
-    then
-        use_ip="$(ifconfig venet0:0 | grep 'inet addr' | head -1 | awk '{print $2}' | awk -F':' '{print $2}')"
-    # Eth1?
-    elif [ "$(ifconfig | grep eth1)" ]
-    then
-        use_ip="$(ifconfig eth1 | grep 'inet addr' | head -1 | awk '{print $2}' | awk -F':' '{print $2}')"
-    # Unknown
-    else
-        use_ip="UNKNOWN"
-    fi
-
-    echo;echo;echo
-    echo -e "\e[00;31mThere was a MySQL connection test problem:\e[00m"
-    echo "$mysql_test_gpx"
-    echo -e "\e[00;31mYour clients wont be able to use FTP until MySQL permissions are granted! "
-    echo "http://gamepanelx.com/docs/index.php?title=Remote_Server_Installation#Master_Database_Privileges"
-    echo
-    echo "You will need to run the following SQL query as root:"
-    echo
-    echo "GRANT ALL ON $gpx_master_mysql_db.* TO '$gpx_master_mysql_user'@'$use_ip' IDENTIFIED BY '$gpx_master_mysql_pass';"
-    echo "FLUSH PRIVILEGES;"
-    echo
-    echo -e "Before running that, make sure the IP Address listed in the query is correct for this server! \e[00m"
-else
-    echo;echo;echo
-    echo -e "\e[00;32mMySQL test successfully connected to the remote database! \e[00m"
+    rm -f /usr/local/gpx/ftpd/pure-ftpd.pid
+    
+    echo -e "\e[00;31mFTP Server failed to start, no running PID found! \e[00m"
 fi
 
 ################
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # GamePanelX
-# Remote Scripts v3.0.10
+# Remote Scripts v3.0.14
 #
 # Installation Script
 #
@@ -11,10 +11,11 @@ echo -e "\e[00;34m##############################################################
 echo "##                                                              ##"
 echo "##                         GamePanelX                           ##"
 echo "##                                                              ##"
-echo "##       Welcome to the Remote Server installer (v3.0.10)       ##"
+echo "##       Welcome to the Remote Server installer (v3.0.14)       ##"
 echo "##                                                              ##"
 echo -e "##################################################################\e[00m"
 echo
+remote_version="3.0.14"
 
 if [ "$UID" -ne "0" ]
 then
@@ -73,19 +74,27 @@ then
 fi
 
 # Check if user already exists
-chk_exist="$(cat /etc/passwd | awk -F':' '{print $1}' | grep $gpx_user)"
-if [ "$chk_exist" ]
+if [ "$(grep "^$gpx_user:" /etc/passwd)" ]
 then
-    echo "ERROR: That user already exists ($chk_exist).  Please choose a different username and try again.  Exiting."
+    echo "ERROR: That user already exists!.  Please choose a different username and try again.  Exiting."
     exit
+fi
+
+# Create the main /usr/local/gpx
+if [ -d /usr/local/gpx ]
+then
+    echo "GPX directory (/usr/local/gpx) already exists!  Please uninstall first if you wish to start over.  Exiting."
+    exit
+else
+    mkdir /usr/local/gpx
 fi
 
 # Create the gpx user
 useradd -m -c "GamePanelX" -s /bin/bash $gpx_user
-gpx_user_home="$(eval echo ~$gpx_user)"
+gpx_user_home="/usr/local/gpx"
 
 # Log this username
-echo $gpx_user > .gpx_lastuser
+echo $gpx_user > $gpx_user_home/.gpx_lastuser
 
 # Make sure homedir exists
 if [ ! -d "$gpx_user_home" ]
@@ -104,13 +113,79 @@ else
 fi
 
 # Change ownership of all the new files
-chown $gpx_user:$gpx_user $gpx_user_home
-chown $gpx_user:$gpx_user $gpx_user_home -R
+chown $gpx_user: $gpx_user_home -R
+chown root:$gpx_user $gpx_user_home/users -R
+chown root: $gpx_user_home/ftpd -R
+chmod 0660 $gpx_user_home/users -R
+chmod 0750 $gpx_user_home/{logs,templates} -R
+chmod 0660 $gpx_user_home/logs/*
+chmod 0700 $gpx_user_home/{addons,queue,tmp,etc,uploads,users} -R
+chmod 0760 $gpx_user_home/queue $gpx_user_home/tmp
+chmod 0774 $gpx_user_home/users
+chmod 0555 $gpx_user_home/bin
+chmod 0754 $gpx_user_home/bin/*
+
+# Setup config
+touch $gpx_user_home/etc/config.cfg
+echo > $gpx_user_home/etc/config.cfg
+> $gpx_user_home/etc/config.cfg
+echo "username: $gpx_user" >> $gpx_user_home/etc/config.cfg
+echo "version: $remote_version" >> $gpx_user_home/etc/config.cfg
 
 # Set system password
 echo
 echo "-- Enter a password for GamePanelX user \"$gpx_user\" "
 passwd $gpx_user
+
+#############################################################################################################
+
+# Only allow gpx* users to login from the master
+if [ "$gpx_master_ip" ]
+then
+    read -p "Modify SSH config to only allow GPX SSH logins from the Master server network? (Highly Recommended) (y/n): " gpx_ssh_answer
+    
+    if [[ "$gpx_ssh_answer" == "y" || "$gpx_ssh_answer" == "yes" || "$gpx_ssh_answer" == "Y" ]]
+    then
+        read -p "Primary Master Server IP: " gpx_master_ip
+        
+        # Split up IP for wildcards
+        ip_net="$(echo $gpx_master_ip | awk -F'.' '{print $1"."$2"."$3}')"
+        
+        # Comment any current AllowUsers lines
+        sed -i 's/^AllowUsers/\# Old\: AllowUsers/g' /etc/ssh/sshd_config
+        
+        # Add our new line
+        echo >> /etc/ssh/sshd_config
+        echo '# Automatically added by GamePanelX' >> /etc/ssh/sshd_config
+        echo "AllowUsers root $gpx_user gpx*@$ip_net*" >> /etc/ssh/sshd_config
+        
+        # Restart SSHD
+        if [ -f /sbin/service ]; then
+            /sbin/service sshd restart
+        elif [ -f /etc/init.d/sshd ]; then
+            /etc/init.d/sshd restart
+        elif [ -f /etc/init.d/ssh ]; then
+            /etc/init.d/ssh restart
+        elif [ -f /etc/rc.d/sshd ]; then
+            /etc/rc.d/sshd restart
+        else
+            echo "Failed to find the SSH server location!  Please manually restart the SSH server."
+        fi
+        
+        echo
+        echo "Modified and restarted the SSH server.  Make sure to edit /etc/ssh/sshd_config if you need any more users allowed."
+    else
+        echo "NOT modifying SSH config."
+    fi
+fi
+
+# Kill old manager processes
+if [ "$(ps -ef | grep GPXManager | grep -v grep)" ]; then
+	killall GPXManager
+fi
+
+# Start the manager daemon
+/usr/local/gpx/bin/GPXManager
 
 #############################################################################################################
 
@@ -120,14 +195,14 @@ read -p "Install GamePanelX FTP server? (y/n): " gpx_ftp_ans
 
 if [[ "$gpx_ftp_ans" == "y" || "$gpx_ftp_ans" == "yes" || "$gpx_ftp_ans" == "Y" ]]
 then
-	if [ ! -f ./ftp.sh ]
-	then
-		echo "No FTP script (./ftp.sh) found!  Exiting."
-		exit
-	fi
+    if [ ! -f ./ftp.sh ]
+    then
+        echo "No FTP script (./ftp.sh) found!  Exiting."
+        exit
+    fi
 
-	chmod u+x ftp.sh
-	./ftp.sh -u $gpx_user
+    chmod u+x ftp.sh
+    ./ftp.sh -u $gpx_user
 fi
 
 echo
